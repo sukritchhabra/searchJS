6eef79079d0187c9543fcdfa220a0af40bef1e2a
Sukrit Chhabra <sukrit.chhabra@gmail.com>
2016-02-20 02:55:28 -0500

    Adding some comments and fixing minor bug with  event creation

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    var json;
-    var debounceTimeout = 200;
-    var searchText = "";
-    var keyupCounter = 0;
+    var searchResult_JSON;      // Search results recieved from ajax request and used to create the search result list
+    var debounceTimeout = 200;  // Global timeout for debouce.
+    var searchText = "";        // Search string the user typed
+    var keyupCounter = 0;       // Counter to set debounce timeout
 
-    var selectedSearchResult = undefined;
-    var selectedSearchString = "";
+    var selectedSearchResult = undefined;   // Final list element that is selected from the search results
+    var selectedSearchString = "";          // The topic that is finally selected
 
 
     $('.searchBar').on('keyup', $.debounce(function(event) {
@@ -25,32 +25,30 @@ $(document).ready(function() {
             console.log('\n\nemptied results');
 
             searchText = $('.searchBar').val();
-            console.log('Search text: ' + searchText);
+            //console.log('Search text: ' + searchText);
 
 
             if (searchText != "") {
+                /* Getting search result for the current search string in the search bar */
                 $.ajax({
                     url: "http://rack36.cs.drexel.edu/suggest/?q=" + searchText,
                     type: "GET",
                     async: false,
                     success: function (response) {
                         // console.log(response);
-                        json = JSON.parse(response);
-                        console.log(json);
+                        searchResult_JSON = JSON.parse(response);
+                        console.log(searchResult_JSON);
                     }
                 });
 
-                var len = json.length;
+                var len = searchResult_JSON.length;     // Number of search results acquired
 
                 $('.results').addClass('active');
                 for (var i = 0; i < len-1; i++) {
-                    var temp = json[i].title;
-                    // console.log("\n\n"+temp);
-                    temp = json[i].title.toLowerCase();
-                    //console.log(temp);
-                    // console.log(temp.indexOf(searchText));
-                    if(temp.indexOf(searchText) >= 0 || json[i].title.indexOf(searchText) >= 0) {
-                        $('.results').append('<li>' + json[i].title + '</li>');
+                    var temp = searchResult_JSON[i].title;
+                    temp = searchResult_JSON[i].title.toLowerCase();
+                    if(temp.indexOf(searchText) >= 0 || searchResult_JSON[i].title.indexOf(searchText) >= 0) {
+                        $('.results').append('<li>' + searchResult_JSON[i].title + '</li>');
                     }
                 }
             }
@@ -63,34 +61,34 @@ $(document).ready(function() {
             var numResults = $('.results li').length;
             var selectedIndex = $('.results li').index($('.selected'));
 
-            if (keyPressed == 40) {                         // If down key is pressed
+            if (keyPressed == 40) {     /* If _DOWN_ key is pressed */
                 if (selectedIndex < 0) {
                     console.log('\n\nundefined');
-                    $('.results li').eq(0).addClass('selected');
+                    $('.results li').eq(0).addClass('selected');    // Select the new element
                     selectedIndex = 0;
                     selectedSearchResult = $('.results .selected');
                 } else {
                     console.log('\n\nprevious selection index: ' + selectedIndex);
-                    $('.results li').eq(selectedIndex).removeClass('selected');
-                    $('.results li').eq((selectedIndex + 1)%numResults).addClass('selected');
+                    $('.results li').eq(selectedIndex).removeClass('selected');     // Remove Selection from previously selected element
+                    $('.results li').eq((selectedIndex + 1)%numResults).addClass('selected');   // Select the new element
                     selectedIndex = (selectedIndex + 1)%numResults;
                     console.log('new selection index: ' + selectedIndex);
                     selectedSearchResult = $('.results .selected');
                 }
-            } else if (keyPressed == 38) {                  // If up key is pressed
+            } else if (keyPressed == 38) {  /* If _UP_ key is pressed */
                 if (selectedIndex == -1) {
-                    $('.results li').eq(numResults - 1).addClass('selected');
+                    $('.results li').eq(numResults - 1).addClass('selected');   // Select the new element
                     selectedIndex = numResults - 1;
                     selectedSearchResult = $('.results .selected');
                 } else {
                     console.log('\n\nprevious selection index: ' + selectedIndex);
-                    $('.results li').eq(selectedIndex).removeClass('selected');
+                    $('.results li').eq(selectedIndex).removeClass('selected');     // Remove Selection from previously selected element
 
                     if (selectedIndex < 1) {
-                        $('.results li').eq( ( numResults - (Math.abs(selectedIndex-1) ) % numResults)).addClass('selected');
+                        $('.results li').eq( ( numResults - (Math.abs(selectedIndex-1) ) % numResults)).addClass('selected');   // Select the new element
                         selectedIndex = numResults - (Math.abs(selectedIndex-1) % numResults);
                     } else {
-                        $('.results li').eq( (selectedIndex - 1) ).addClass('selected');
+                        $('.results li').eq( (selectedIndex - 1) ).addClass('selected');    // Select the new element
                         selectedIndex = selectedIndex - 1;
 
                     }
@@ -104,8 +102,8 @@ $(document).ready(function() {
             $('.results').empty();
 
             /* Creating search event */
-            var searchEvent = jQuery.event("search");
-            searchEvent.searchString = selectedSearchString;
+            var searchEvent = $.event("search");
+            searchEvent.searchString = selectedSearchString;    // Adding key searchString to search event
             $('body').trigger(searchEvent);
         }
     });
